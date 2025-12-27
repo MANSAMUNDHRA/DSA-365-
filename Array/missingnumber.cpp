d2a94af40be79f54549243c885a99be1900d656b
MANSAMUNDHRA <mansha.mundhra2005@gmail.com>
2025-12-27 17:56:45 +0530

    27-12-25

---
+++ Array/missingnumber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int actual= (n*(n+1))/2;
+        int sum=0;
+        for (int i:nums){
+            sum=sum+i;
+        }
+        int missing = actual-sum;
+        return missing;
+    }
+};
